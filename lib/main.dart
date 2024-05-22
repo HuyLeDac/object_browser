2a75523594afd897bb0b3188038bd5b2c83a6f8c
HuyLeDac <huyledac04@gmail.com>
2024-05-22 14:29:18 +0200

    Made the application more stable against render overflow

--- lib/main.dart
+++ lib/main.dart
@@ -38,6 +38,166 @@ final tree = TreeNode<UserName>.root(data: UserName("User", "Names"))
     TreeNode<UserName>(key: "0E", data: UserName("Mr. Smith", "Agent")),
   ]);
 
+//Dummy Tree
+final table = 
+  Card(
+    child: Column( // Table dummy, use DataTable
+      children: <Widget>[
+        const Text('Table'), 
+        DataTable(  
+          columns: const [
+            DataColumn(
+              label: Text('Nr:'),
+            ),
+            DataColumn(
+              label: Text('Gruppe'),
+            ),
+            DataColumn(
+              label: Text('Offset:'),
+            ),
+            DataColumn(
+              label: Text('Bezeichner:'),
+            ),
+            DataColumn(
+              label: Text('Datentyp:'),
+            ),
+            DataColumn(
+              label: Text('Länge:'),
+            ),
+            DataColumn(
+              label: Text('Einheit:'),
+            ),
+            DataColumn(
+              label: Text('Wert:'),
+            ),
+          ], 
+          rows: const [        
+            DataRow(cells: [
+              DataCell(Text('1180416')),
+              DataCell(Text('cycle time')),
+              DataCell(Text('5644645')),
+              DataCell(Text('1180416')),
+              DataCell(Text('cycle time')),
+              DataCell(Text('5644645')),
+              DataCell(Text('1180416')),
+              DataCell(Text('cycle time')),
+            ]),
+            DataRow(cells: [
+              DataCell(Text('1180416')),
+              DataCell(Text('cycle time')),
+              DataCell(Text('5644645')),
+              DataCell(Text('1180416')),
+              DataCell(Text('cycle time')),
+              DataCell(Text('5644645')),
+              DataCell(Text('1180416')),
+              DataCell(Text('cycle time')),
+            ]),
+            DataRow(cells: [
+              DataCell(Text('1180416')),
+              DataCell(Text('cycle time')),
+              DataCell(Text('5644645')),
+              DataCell(Text('1180416')),
+              DataCell(Text('cycle time')),
+              DataCell(Text('5644645')),
+              DataCell(Text('1180416')),
+              DataCell(Text('cycle time')),
+            ]),
+            DataRow(cells: [
+              DataCell(Text('1180416')),
+              DataCell(Text('cycle time')),
+              DataCell(Text('5644645')),
+              DataCell(Text('1180416')),
+              DataCell(Text('cycle time')),
+              DataCell(Text('5644645')),
+              DataCell(Text('1180416')),
+              DataCell(Text('cycle time')),
+            ]),
+            DataRow(cells: [
+              DataCell(Text('1180416')),
+              DataCell(Text('cycle time')),
+              DataCell(Text('5644645')),
+              DataCell(Text('1180416')),
+              DataCell(Text('cycle time')),
+              DataCell(Text('5644645')),
+              DataCell(Text('1180416')),
+              DataCell(Text('cycle time')),
+            ]),
+            DataRow(cells: [
+              DataCell(Text('1180416')),
+              DataCell(Text('cycle time')),
+              DataCell(Text('5644645')),
+              DataCell(Text('1180416')),
+              DataCell(Text('cycle time')),
+              DataCell(Text('5644645')),
+              DataCell(Text('1180416')),
+              DataCell(Text('cycle time')),
+            ]),
+            DataRow(cells: [
+              DataCell(Text('1180416')),
+              DataCell(Text('cycle time')),
+              DataCell(Text('5644645')),
+              DataCell(Text('1180416')),
+              DataCell(Text('cycle time')),
+              DataCell(Text('5644645')),
+              DataCell(Text('1180416')),
+              DataCell(Text('cycle time')),
+            ]),
+            DataRow(cells: [
+              DataCell(Text('1180416')),
+              DataCell(Text('cycle time')),
+              DataCell(Text('5644645')),
+              DataCell(Text('1180416')),
+              DataCell(Text('cycle time')),
+              DataCell(Text('5644645')),
+              DataCell(Text('1180416')),
+              DataCell(Text('cycle time')),
+            ]),
+            DataRow(cells: [
+              DataCell(Text('1180416')),
+              DataCell(Text('cycle time')),
+              DataCell(Text('5644645')),
+              DataCell(Text('1180416')),
+              DataCell(Text('cycle time')),
+              DataCell(Text('5644645')),
+              DataCell(Text('1180416')),
+              DataCell(Text('cycle time')),
+            ]),
+            DataRow(cells: [
+              DataCell(Text('1180416')),
+              DataCell(Text('cycle time')),
+              DataCell(Text('5644645')),
+              DataCell(Text('1180416')),
+              DataCell(Text('cycle time')),
+              DataCell(Text('5644645')),
+              DataCell(Text('1180416')),
+              DataCell(Text('cycle time')),
+            ]),
+            DataRow(cells: [
+              DataCell(Text('1180416')),
+              DataCell(Text('cycle time')),
+              DataCell(Text('5644645')),
+              DataCell(Text('1180416')),
+              DataCell(Text('cycle time')),
+              DataCell(Text('5644645')),
+              DataCell(Text('1180416')),
+              DataCell(Text('cycle time')),
+            ]),
+            DataRow(cells: [
+              DataCell(Text('1180416')),
+              DataCell(Text('cycle time')),
+              DataCell(Text('5644645')),
+              DataCell(Text('1180416')),
+              DataCell(Text('cycle time')),
+              DataCell(Text('5644645')),
+              DataCell(Text('1180416')),
+              DataCell(Text('cycle time')),
+            ]),
+          ],
+        ),
+      ]
+    ),
+  );
+
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -51,146 +211,95 @@ class HomeScreen extends StatelessWidget {
         title: const Text('ObjectBrowser'),
       ),
 
-      body: Column(
-        children: <Widget>[ 
-          Row(
-            // Load button, add other buttons later 
-            children: <Widget>[
-              const SizedBox(width: 20), // Adds horizontal space of 20 pixels
-
-              ElevatedButton(
-                onPressed: () {
-                  showDialog(
-                    context: context,
-                    builder: (BuildContext context) {
-                      return const LoadingDialog(); // Show loading dialog when button is pressed
-                    },
-                  );
-                },
-                child: const Text('Load CNC objects'),
-              ),
-
-              const SizedBox(width: 20), // Adds horizontal space of 20 pixels
-
-              ElevatedButton(
-                onPressed: () {
-                  showDialog(
-                    context: context,
-                    builder: (BuildContext context) {
-                      return const LoadingDialog(); // Dummy for modifying DLL paths
-                    },
-                  );
-                },
-                child: const Text('Modify DLL path'),
-              ),
-
-              const SizedBox(width: 20), // Adds horizontal space of 20 pixels
-
-              ElevatedButton(
-                onPressed: () {
-                  showDialog(
-                    context: context,
-                    builder: (BuildContext context) {
-                      return const LoadingDialog(); // Dummy for other buttons 
-                    },
-                  );
-                },
-                child: const Text('...'),
-              ),
-            ]
-
-          ),
-          
-          // Two columns, one for tree and one for table which should illustrate all objects
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-
-              const SizedBox(width: 20), // Adds horizontal space of 20 pixels
-
-              //const Card(
-              //  child: Text('Tree'), //Tree dummy, implement Tree with flutter_fancy_tree_view,
-              //),
-              Expanded(
-                child:TreeView.simpleTyped<UserName, TreeNode<UserName>>(
-                  tree: tree,
-                  expansionBehavior: ExpansionBehavior.collapseOthersAndSnapToTop,
-                  shrinkWrap: true,
-                  builder: (context, node) => Card(
-                    child: ListTile(
-                      title: Text("Item ${node.level}-${node.key}"),
-                      subtitle: Text('${node.data?.firstName} ${node.data?.lastName}'),
+      body: SingleChildScrollView( // Avoid Render Overflow
+        child: Column(
+          children: <Widget>[ 
+            Row(
+              // Load button, add other buttons later 
+              children: <Widget>[
+                const SizedBox(width: 20), // Adds horizontal space of 20 pixels
+        
+                ElevatedButton(
+                  onPressed: () {
+                    showDialog(
+                      context: context,
+                      builder: (BuildContext context) {
+                        return const LoadingDialog(); // Show loading dialog when button is pressed
+                      },
+                    );
+                  },
+                  child: const Text('Load CNC objects'),
+                ),
+        
+                const SizedBox(width: 20), 
+        
+                ElevatedButton(
+                  onPressed: () {
+                    showDialog(
+                      context: context,
+                      builder: (BuildContext context) {
+                        return const LoadingDialog(); // Dummy for modifying DLL paths
+                      },
+                    );
+                  },
+                  child: const Text('Modify DLL path'),
+                ),
+        
+                const SizedBox(width: 20), // Adds horizontal space of 20 pixels
+        
+                ElevatedButton(
+                  onPressed: () {
+                    showDialog(
+                      context: context,
+                      builder: (BuildContext context) {
+                        return const LoadingDialog(); // Dummy for other buttons 
+                      },
+                    );
+                  },
+                  child: const Text('...'),
+                ),
+              ]
+        
+            ),
+            
+            // Add horizontal spacing
+            const SizedBox(height: 20),
+        
+            // Two columns, one for tree and one for table which should illustrate all objects
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+        
+                const SizedBox(width: 20), // Adds horizontal space of 20 pixels
+        
+                //Tree Dummy
+                Expanded(
+                  child: SingleChildScrollView(
+                    child: TreeView.simpleTyped<UserName, TreeNode<UserName>>(
+                      tree: tree,
+                      expansionBehavior: ExpansionBehavior.snapToTop,
+                      shrinkWrap: true,
+                      builder: (context, node) => Card(
+                        child: ListTile(
+                          title: Text("Item ${node.level}-${node.key}"),
+                          subtitle: Text('${node.data?.firstName} ${node.data?.lastName}'),
+                        ),
+                      ),
                     ),
                   ),
                 ),
-              ),
-
-
-              const SizedBox(width: 20), // Adds horizontal space of 20 pixels
-
-              Card(
-                child: Column( // Table dummy, use DataTable
-                  children: <Widget>[
-                    const Text('Table'), 
-                    DataTable(  
-                      columns: const [
-                        DataColumn(
-                          label: Text('Nr:'),
-                        ),
-                        DataColumn(
-                          label: Text('Gruppe'),
-                        ),
-                        DataColumn(
-                          label: Text('Offset:'),
-                        ),
-                        DataColumn(
-                          label: Text('Bezeichner:'),
-                        ),
-                        DataColumn(
-                          label: Text('Datentyp:'),
-                        ),
-                        DataColumn(
-                          label: Text('Länge:'),
-                        ),
-                        DataColumn(
-                          label: Text('Einheit:'),
-                        ),
-                        DataColumn(
-                          label: Text('Wert:'),
-                        ),
-                      ], 
-                      rows: const [        
-                        DataRow(cells: [
-                          DataCell(Text('1180416')),
-                          DataCell(Text('cycle time')),
-                          DataCell(Text('5644645')),
-                          DataCell(Text('1180416')),
-                          DataCell(Text('cycle time')),
-                          DataCell(Text('5644645')),
-                          DataCell(Text('1180416')),
-                          DataCell(Text('cycle time')),
-                        ]),
-                        DataRow(cells: [
-                          DataCell(Text('1180416')),
-                          DataCell(Text('cycle time')),
-                          DataCell(Text('5644645')),
-                          DataCell(Text('1180416')),
-                          DataCell(Text('cycle time')),
-                          DataCell(Text('5644645')),
-                          DataCell(Text('1180416')),
-                          DataCell(Text('cycle time')),
-                        ])
-                      ],
-                    ),
-                  ]
-                ),
-              ),
-
-              const SizedBox(width: 20), // Adds horizontal space of 20 pixels
-
-            ],
-          )
-        ]
+        
+                const SizedBox(width: 20), // Adds horizontal space of 20 pixels
+        
+                table, //Dummy table
+        
+                const SizedBox(width: 20), // Adds horizontal space of 20 pixels
+        
+              ],
+            )
+          ]
+        ),
       ),
     );
   }
